9ef8f64a510f5295ae11d943ba1ec3657b9ddc4e
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2021-05-20 23:26:43 +0300

    Add props example

--- index.js
+++ index.js
@@ -6,15 +6,29 @@ ReactDOM
 const container = document.getElementById("root");
 
 class Heading extends React.Component {
-  render(){
-    return React.createElement("h1", {
-      title: "Hello react!",
-      className: "heading",
-    },
-     "Hello React.js!");
+  render() {
+    console.log(this);
+    const { name, classNameForHeading, titleForHeading, children} = this.props;
+    return React.createElement(
+      "h1",
+      {
+        title: titleForHeading,
+        className: classNameForHeading,
+      },
+      `Hello ${name}!`,
+      ...children,
+    );
   }
 }
 
-const reactElement = React.createElement(Heading);
+const reactElement = React.createElement(Heading, {
+  name: "React",
+  titleForHeading: 'Hello Everyone!',
+  classNameForHeading: "heading test",
+},
+"test",
+"lorem",
+"something",
+);
 
 ReactDOM.render(reactElement, container);
